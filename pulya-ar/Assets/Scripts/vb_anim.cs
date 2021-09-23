d72c3d97507a3c30f6cbd1189dd1a055e516b6b6
SentDyem <55694826+SentDyem@users.noreply.github.com>
2021-09-23 23:24:31 +0300

    init

---
+++ pulya-ar/Assets/Scripts/vb_anim.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using Vuforia;
+
+public class vb_anim : MonoBehaviour
+{
+    public VirtualButtonBehaviour vb, actionBtn;
+    public GameObject info;
+    bool pressed = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // btn = GameObject.Find("VirtualButton");
+        vb.RegisterOnButtonPressed(btnPressed);
+        vb.RegisterOnButtonReleased(btnReleased);
+    }
+
+    private void btnReleased(VirtualButtonBehaviour obj)
+    {
+
+       // info.SetActive(true);
+        
+    }
+
+    private void btnPressed(VirtualButtonBehaviour obj)
+    {
+        if (pressed == false)
+        {
+            info.SetActive(true);
+            pressed = true;
+        }
+        else
+        {
+           info.SetActive(false);
+             pressed = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
